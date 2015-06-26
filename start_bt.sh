6ba89f4adb1dd0f1f3c8caa104e132e824947ea0
Larry Finger <Larry.Finger@lwfinger.net>
2015-06-26 11:32:16 -0500

    rtl8723bs_bt: Change script to select the TTY device
    Now that we know what info the kernel logs when the BT part of an RTL8723BS
    is discovered, the startup script is modified to detect and select that device.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- start_bt.sh
+++ start_bt.sh
@@ -3,12 +3,26 @@
 # Shell script to install Bluetooth firmware and attach BT part of
 # RTL8723BS
 #
-TTY=/dev/ttyS4
+# Find the TTY attached to the BT device
+TTYSTRING=`dmesg -t | grep tty | grep MMIO | cut -b 14-18`
+TTY=`expr substr "$TTYSTRING" 1 5`
 
-if [ -f /lib/firmware/rtlbt/rtlbt_config ];
+if [ "$TTYSTRING" = "" ]
 then
-	echo "Firmware already loaded"
-else
+	echo
+	echo "No BT TTY device has been found"
+	echo "Either this computer has no BT device that uses hciattach, or"
+	echo "Your kernel does not have module 8250_dw configured."
+	echo "Note: The configuration variable is CONFIG_SERIAL_8250_DW."
+	echo
+	exit 1
+fi
+
+TTY="/dev/$TTY"
+echo "Using device $TTY for Bluetooth"
+
+if [ ! -f /lib/firmware/rtlbt/rtlbt_config ];
+then
 	mkdir -p /lib/firmware/rtlbt/
 	cp rtlbt_* /lib/firmware/rtlbt/.
 fi
